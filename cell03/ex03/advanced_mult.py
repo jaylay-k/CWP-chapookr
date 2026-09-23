d43f8a85b331587f8ef5704c174aaa29f42b7e20
chapookr <68050074@kmitl.ac.th>
2026-09-23 15:06:20 +0700

    update

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,5 +1,17 @@
-for table in range(11) :
-    print("Table de", table, end=":")
-    for i in range(11) :
-        print(table * i, end=" ")
-    print()
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+  
+    i = 0
+    while i <= 10:
+        print(f"Table de {i}:", end="")
+    
+        j = 0
+        while j <= 10:
+            print(f" {i * j}", end="")
+            j += 1
+            
+        print() 
+        i += 1
